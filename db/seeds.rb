b8644de4c58cadc76ae1777884f4fbadee7334e3
Jonathan East <jonathan_east@ymail.com>
2021-04-08 08:56:11 -0400

    added seed data to workout; tested data visible on rails server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Workout.destroy_all
+
+workout1 = Workout.create(name: "Strength")
+workout2 = Workout.create(name: "Back")
+workout3 = Workout.create(name: "Legs")
