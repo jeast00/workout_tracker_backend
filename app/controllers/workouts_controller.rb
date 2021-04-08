7566ce1c48106bfacd1b1af5f594bdba678fa2cd
Jonathan East <jonathan_east@ymail.com>
2021-04-08 11:06:29 -0400

    changed configuration origin for cors.rb

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,7 +2,7 @@ class WorkoutsController < ApplicationController
 
     def index
         # byebug - tested index through rails server and byebug
-        render json: Workout.all, :include => :exercises #check localhost to view data under workouts route
+        render json: Workout.all, include: :exercises #check localhost to view data under workouts route
     end
 
 end
