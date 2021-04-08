b6d9874633bd4cabd11fdf8f2e6b53d6f548281d
Jonathan East <jonathan_east@ymail.com>
2021-04-08 10:00:57 -0400

    updated the index method in WorkoutsController to include exercises; tested data in rails server

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,7 +2,7 @@ class WorkoutsController < ApplicationController
 
     def index
         # byebug - tested index through rails server and byebug
-        render json: Workout.all 
+        render json: Workout.all, :include => :exercises
     end
 
 end
